dfb8643c64e34554a8c4e0acf7557d18ba7905fb
Antony Male <antony.male@gmail.com>
2024-07-25 09:29:39 +0100

    Support adding additional static libraries

--- flame-map.py
+++ flame-map.py
@@ -26,7 +26,7 @@ class ElfFlameGenerator:
         section = symbol['section']
         category = next((k for k, v in self._section_categories.items() if section in v), None) 
         if not category:
-            print(f"Found unmapped section: {section}", file=sys.stderr)
+            print(f"Found unmapped section: {section}. Specify mapping with -s", file=sys.stderr)
             return
 
         node = tree[category]
@@ -39,52 +39,60 @@ class ElfFlameGenerator:
         file_node = node.setdefault(section, {})
         file_node[name] = symbol['size']
 
-    def generate(self, elf, output):
+    def generate(self, elf, static_libraries, output):
         sections = {}
         symbols = {}
 
-        p = subprocess.Popen([self._readelf, "--symbols", "--wide", elf], stdout=subprocess.PIPE)
-        for line in p.stdout:
-            match = re.fullmatch(r"\s*(\d+):\s+(?P<addr>\w+)\s+(?P<size>(0x)?\d+)\s+(?P<type>\w+)\s+(?P<bind>\w+)\s+(?P<vis>\w+)\s+(?P<ndx>\w+)\s+(?P<name>.+)", line.decode('utf-8').strip())
-            if not match:
-                continue
+        with subprocess.Popen([self._readelf, "--symbols", "--wide", "--sym-base=10", elf], stdout=subprocess.PIPE) as p:
+            for line in p.stdout:
+                match = re.fullmatch(r"\s*(\d+):\s+(?P<addr>\w+)\s+(?P<size>\d+)\s+(?P<type>\w+)\s+(?P<bind>\w+)\s+(?P<vis>\w+)\s+(?P<ndx>\w+)\s+(?P<name>.+)", line.decode('utf-8').strip())
+                if not match:
+                    continue
 
-            # All of the sections should appear before anything else
-            if match['type'] == 'SECTION':
-                sections[match['ndx']] = match['name']
-            elif match['size'] != '0':
-                symbols[match['name']] = {
-                    'section': sections[match['ndx']],
-                    'addr': match['addr'],
-                    'size': int(match['size'], 0),
-                    'file': None,
-                    'found_in_nm': False,
-                }
+                # All of the sections should appear before anything else
+                if match['type'] == 'SECTION':
+                    sections[match['ndx']] = match['name']
+                elif match['size'] != '0':
+                    symbols[match['name']] = {
+                        'section': sections[match['ndx']],
+                        'addr': match['addr'],
+                        'size': int(match['size']),
+                        'found_in_nm': False,
+                    }
 
         tree = {}
         for category in self._section_categories:
             tree[category] = {}
 
-        p = subprocess.Popen([self._nm, "--line-numbers", elf], stdout=subprocess.PIPE)
-        for line in p.stdout:
-            match = re.fullmatch(r"(?P<addr>\w+)\s+(?P<section>\w+)\s+(?P<name>\S+)\s+(?P<file>[^:]+):(?P<line>\d+)", line.decode('utf-8').strip())
-            if not match:
-                continue
+        print(" ".join([self._nm, "--line-numbers", "--print-file-name", elf, *static_libraries]))
+        with subprocess.Popen([self._nm, "--line-numbers", "--print-file-name", elf, *static_libraries], stdout=subprocess.PIPE) as p:
+            for line in p.stdout:
+                match = re.fullmatch(r"(?P<infile>[^:]+):(?:(?P<object>[^:]+):)?(?P<addr>\w+)\s+(?P<section>\w+)\s+(?P<name>\S+)(?:\s+(?P<file>.+?):(?P<line>\d+))?", line.decode('utf-8').strip())
+                if not match:
+                    continue
 
-            symbol = symbols.get(match['name'])
-            if not symbol or symbol['addr'] != match['addr']:
-                continue
+                symbol = symbols.get(match['name'])
+                if not symbol:
+                    continue
 
-            symbol['found_in_nm'] = True
+                # If it doesn't have a file path, then use the library / object name (if it exists) as the path
+                if match['file']:
+                    path_parts = Path(match['file']).resolve().parts
+                elif match['infile'] != elf:
+                    path_parts = list(Path(match['infile']).resolve().parts)
+                    if match['object']:
+                        path_parts.append(match['object'])
+                else:
+                    continue
 
-            
-            path_parts = Path(match['file']).resolve().parts
-            # If any root dir appear in this path, then strip everything before it
-            for root_dir in self._root_dirs:
-                if root_dir in path_parts:
-                    path_parts = path_parts[path_parts.index(root_dir):]
+                symbol['found_in_nm'] = True
 
-            self._add_symbol_to_tree(tree, symbol, match['name'], path_parts)
+                # If any root dir appear in this path, then strip everything before it
+                for root_dir in self._root_dirs:
+                    if root_dir in path_parts:
+                        path_parts = path_parts[path_parts.index(root_dir):]
+
+                self._add_symbol_to_tree(tree, symbol, match['name'], path_parts)
 
         for name, symbol in symbols.items():
             if not symbol['found_in_nm']:
@@ -98,8 +106,9 @@ class ElfFlameGenerator:
                 else:
                     yield from _visit_node(sorted(node.items()), node_path)
 
+        elf_name = Path(elf).name
         for path, size in _visit_node(tree.items(), []):
-            output.write(f"{elf};" + ";".join(path) + f" {size}\n")
+            output.write(f"{elf_name};" + ";".join(path) + f" {size}\n")
 
 class ParseDict(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
@@ -119,6 +128,7 @@ if __name__ == "__main__":
     )
     parser.add_argument('file', metavar='file.elf', help='Path to the elf to process')
     parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout, help='File to write to (defaults to stdout)')
+    parser.add_argument('-a', '--library', metavar='STATIC_LIBRARY', dest='libraries', action='append', default=[], help='Specify additional static libraries to find symbols in')
     parser.add_argument('-s', '--section', metavar='name=sec1,sec2', nargs='+', dest='sections', action=ParseDict, default={}, help='Add rules for grouping sections into flash/ram/etc')
     parser.add_argument('-r', '--root', metavar='ROOT', dest='roots', action='append', default=[], help='Add folders which are considered root folders')
     parser.add_argument('--nm', default='nm', help='Path to nm.exe')
@@ -127,4 +137,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     generator = ElfFlameGenerator(args.sections, args.roots, args.nm, args.readelf)
-    generator.generate(args.file, args.output)
+    generator.generate(args.file, args.libraries, args.output)
